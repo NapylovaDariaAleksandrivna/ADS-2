3cd7898dc6405ddbc2f7b5da43b51793d60362bc
NapylovaDariaAleksandrivna <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-02-27 12:47:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,52 @@
 #include <cstdint>
 #include "alg.h"
 
+/*
+ - double pown(double value, uint16_t n) - возведение числа в целую степень.
+   - uint64_t fact(uint16_t n) - вычисление факториала чила n.
+   - double calcItem(double x, uint16_t n) - вычисление члена ряда.
+   - double expn(double x, uint16_t count) - вычисление значения функции exp(x) с суммированием count - членов ряда.
+   - double sinn(double x, uint16_t count) - вычисление значения функции sin(x) с суммированием count - членов ряда.
+   - double cosn(double x, uint16_t count) - вычисление значения функции cos(x) с суммированием count - членов ряда.
+*/
 
 double pown(double value, uint16_t n) {
-
+	if (n == 1) {
+		return value;
+	}
+	return value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-
+	if (n == 1) {
+		return n;
+	}
+	return n * fact( n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+	return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+	double res = 0;
+	for (int i = 0; i <= count; i++) {
+		res += calcItem(x,i);
+	}
+	return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+	double res = 0;
+	for (int i = 1; i <= count; i++) {
+		res += calcItem(x, 2 * i - 1) * pown(-1, i + 1);
+	}
+	return res;
 }
 
 double cosn(double x, uint16_t count) {
-
-}
+	double res = 0;
+	for (int i = 0; i <= count; i++) {
+		res += calcItem(x, 2 * i ) * pown(-1, i + 2);
+	}
+	return res;
