0ddc2e9aa8317944e72c3639dade8a4fed900759
NapylovaDariaAleksandrivna <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-03-01 11:33:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,7 +30,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double res = 0;
-  for (int i = 1; i < count; i++) {
+  for (int i = 1; i <= count; i++) {
     res += calcItem(x, 2 * i - 1) * pown(-1, i + 1);
   }
   return res;
